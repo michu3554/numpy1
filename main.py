ce36aadffb92f565d8c2bb150077380e1dd4eded
michu3554 <variuxpl@gmail.com>
2021-04-25 22:24:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+# Zad1
+
+# a = np.arange(3, 48, 3)
+# print(a)
+
+# Zad2
+
+# a = np.arange(1, 2, 0.1)
+# print(a)
+# b = a.astype('int64')
+# print(a.dtype)
+# print(b.dtype)
+
+# Zad3
+
+# def macierz(n):
+#     for i in range(n):
+#         x = n * n + 1
+#         a = np.arange(1, x).reshape(n, n)
+#     print(a)
+#
+#
+# macierz(2)
+# macierz(5)
+
+# Zad4
+
+# def generuj(x, y):
+#     a = np.logspace(1, y, num=y, base=x, dtype=int)
+#     print(a)
+#
+#
+# generuj(2, 4)
+# generuj(2, 10)
+# generuj(5, 6)
+
+
